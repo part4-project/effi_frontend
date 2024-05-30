34a36c8fbe42be175227da5520bac0c6adba76fd
dali1999 <dali1999@naver.com>
2024-05-30 16:37:34 +0900

    feat: 그룹 홈 side-bar 추가

--- src/pages/group-home/index.tsx
+++ src/pages/group-home/index.tsx
@@ -1,35 +1,19 @@
-import arrowRight from '@assets/icons/arrow-left.svg';
-import GroupModal from '@components/modal/group-modal';
-import MeetingModal from '@components/modal/meeting-modal';
 import { navBarHeight } from '@styles/subsection-size';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import GroupHomeHeader from './components/group-home-header';
+import GroupHomeSideBar from './components/group-home-sidebar';
 import MeetingNotes from './components/meeting-notes';
 import Meetings from './components/meetings';
 
 const GroupHome = () => {
-  const navigate = useNavigate();
-
-  const handleBackButtonClick = () => {
-    navigate('/');
-  };
-
   return (
     <S.Container>
-      <S.GroupHomeHeader>
-        <S.BackButton onClick={handleBackButtonClick} />
-        <S.ManageButtons>
-          <GroupModal>
-            <div>그룹 관리</div>
-          </GroupModal>
-          <MeetingModal>
-            <div>회의 생성</div>
-          </MeetingModal>
-        </S.ManageButtons>
-      </S.GroupHomeHeader>
-
-      <Meetings />
-      <MeetingNotes />
+      <GroupHomeSideBar />
+      <S.GroupHomeMain>
+        <GroupHomeHeader />
+        <Meetings />
+        <MeetingNotes />
+      </S.GroupHomeMain>
     </S.Container>
   );
 };
@@ -38,12 +22,22 @@ export default GroupHome;
 
 const S = {
   Container: styled.div`
+    display: flex;
+  `,
+
+  SideBar: styled.div`
+    background-color: rgba(0, 0, 0, 0.1);
+    width: 270px;
+  `,
+
+  GroupHomeMain: styled.div`
+    width: 100%;
+
     height: calc(100vh - ${navBarHeight.desktop});
     padding: 30px;
     overflow: hidden;
     display: flex;
     flex-direction: column;
-
     button {
       background-color: rgba(0, 0, 0, 0.1);
       padding: 10px 15px;
@@ -53,23 +47,5 @@ const S = {
         background-color: rgba(0, 0, 0, 0.14);
       }
     }
-  `,
-
-  GroupHomeHeader: styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `,
-
-  BackButton: styled.img.attrs({
-    src: arrowRight,
-  })`
-    width: 15px;
-    cursor: pointer;
-  `,
-
-  ManageButtons: styled.div`
-    display: flex;
-    gap: 20px;
   `,
 };
