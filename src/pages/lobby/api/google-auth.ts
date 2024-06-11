7d2e3356af5cc6f69575581494a798688fb36734
dali1999 <dali1999@naver.com>
2024-06-11 17:51:56 +0900

    feat: 헤더 이름 소문자 변경

--- src/pages/lobby/api/google-auth.ts
+++ src/pages/lobby/api/google-auth.ts
@@ -8,11 +8,12 @@ const GoogleOAuth = () => {
 
   const loginMutation = useMutation({
     mutationFn: async (code: string) => {
-      return await axios.get(`${import.meta.env.VITE_SERVER_URL}/user/login?code=${code}`);
+      return await axios.get(`${import.meta.env.VITE_SERVER_URL}///////user/login?code=${code}`);
     },
     onSuccess: (response) => {
-      console.log(response);
-      const accessToken = response.headers['Authorization'].split(' ')[1];
+      // console.log(response);
+      const accessToken = response.headers['authorization'];
+      console.log(accessToken);
       setCookie('accessToken', accessToken);
       //navigate('/');
     },
