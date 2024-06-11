5f7e7fc59366ce6a575058ccc86d4ad8bd72472e
bumpy1800 <bumpy1800@gmail.com>
2024-06-12 00:43:40 +0900

    feat: 쿠키로직 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,4 @@
+import { getCookie } from '@utils/cookie';
 import axios from 'axios';
 
 const instance = axios.create({
@@ -7,7 +8,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    const token = getToken();
+    const token = getCookie('accessToken');
 
     config.headers['Authorization'] = `Bearer ${token}`;
 
