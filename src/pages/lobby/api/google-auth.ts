0a91c3dc862bb5334f3eb6bd320d76cf61007df1
dali1999 <dali1999@naver.com>
2024-06-11 19:05:46 +0900

    feat: accessToken header 테스트

--- src/pages/lobby/api/google-auth.ts
+++ src/pages/lobby/api/google-auth.ts
@@ -1,25 +1,20 @@
 import { useMutation } from '@tanstack/react-query';
-// import { setCookie } from '@utils/cookie';
+import { setCookie } from '@utils/cookie';
+
 import axios from 'axios';
-//import { useNavigate } from 'react-router-dom';
 
 const GoogleOAuth = () => {
-  //const navigate = useNavigate();
-
   const loginMutation = useMutation({
     mutationFn: async (code: string) => {
       return await axios.get(`${import.meta.env.VITE_SERVER_URL}/user/login?code=${code}`);
     },
     onSuccess: (response) => {
-      console.log(response);
-      // const accessToken = response.headers.authorization;
-      // console.log(accessToken);
-      // setCookie('accessToken', accessToken);
-      //navigate('/');
+      const accessToken = response.headers.authorization.split[' '][1];
+      console.log(accessToken);
+      setCookie('accessToken', accessToken);
     },
     onError: (error) => {
       console.error(error);
-      //navigate('/login');
     },
   });
 
