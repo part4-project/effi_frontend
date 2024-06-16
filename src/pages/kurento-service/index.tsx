d2593178ff70cddc80f9f7f6921fd21a93d18ce2
Jules <dali1999@naver.com>
2024-06-16 12:27:25 +0900

    feat: 방 입장, 퇴장, 채팅 메시지 전송 테스트 완료

--- src/pages/kurento-service/index.tsx
+++ src/pages/kurento-service/index.tsx
@@ -1,68 +1,125 @@
-import { useEffect, useRef } from 'react';
-import { Client } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import WebSocketService from './utils/kurento-service';
 
 const KurentoService = () => {
-  const stompClientRef = useRef(null);
+  const [title, setTitle] = useState('EFFI 채팅 테스트');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [roomId, setRoomId] = useState(1); // roomId 임시
+  const [userId, setUserId] = useState(123); //  userId 임시
+  const [meetingId, setMeetingId] = useState(456); // meetingId 임시
 
   useEffect(() => {
-    const sock = new SockJS('https://api.effi.club/signal/connect'); // WebSocket 엔드포인트
-    const stompClient = new Client({ webSocketFactory: () => sock });
-
-    stompClientRef.current = stompClient;
-
-    stompClient.onConnect = () => {
-      // console.log(new Date());
-      console.log('STOMP 연결됨');
-      // 엔드포인트 추후 /signal/sub/meeting/1/chat 으로 바뀔예정 (반영아직안됨);
-      stompClient.subscribe('/signal/sub/1/chat', handleReceivedMessage); // 구독할 주제 설정
-    };
-
-    stompClient.onDisconnect = () => {
-      console.log('STOMP 연결 끊어짐');
-    };
-
-    stompClient.activate();
+    WebSocketService.connect((msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
 
     return () => {
-      if (stompClient.connected) {
-        console.log('자동해제');
-        stompClient.deactivate();
-      }
+      WebSocketService.disconnect();
     };
   }, []);
+  console.log(messages);
 
-  const handleReceivedMessage = (message) => {
-    console.log('서버로부터 메시지 수신:', message.body);
-    // 원하는 동작 수행
+  const handleEnter = () => {
+    setTitle(`${roomId}방 접속`);
+    console.log(document.getElementById('roomTitle'));
+    document.getElementById('enterBtn').style.visibility = 'hidden';
+    document.getElementById('exitBtn').style.visibility = 'visible';
+    document.getElementById('messages').style.visibility = 'visible';
+
+    const sessionDto = { meetingId, userId };
+    WebSocketService.sendMessage(`/signal/pub/meeting/${roomId}/enter`, sessionDto);
   };
 
-  const sendMessageToServer = () => {
-    const messageObject = {
-      type: 'ENTER', // SessionMessageType에 따라 적절한 값을 설정
-      userId: 30, // 실제 사용자 ID 값
-      meetingId: 1, // 회의 ID 값
-      message: ' ID : 30번 유저가 1번 회의실에 입장하였습니다!!', // 원하는 메시지
-      timeStamp: new Date().toISOString(), // 현재 시각 ISO 문자열 형식으로
-    };
+  const handleSendMessage = () => {
+    const sessionDto = { meetingId, userId, message };
+    WebSocketService.sendMessage(`/signal/pub/meeting/${roomId}/chat`, sessionDto);
+    setMessage('');
+  };
 
-    if (stompClientRef.current.connected) {
-      stompClientRef.current.publish({
-        destination: '/signal/pub/meeting/1/enter',
-        body: JSON.stringify(messageObject),
-      });
-      // console.log('Message sent to server:', messageObject);
-    } else {
-      console.error('WebSocket 연결이 필요합니다.');
-    }
+  const handleLeave = () => {
+    setTitle(`EFFI 채팅 테스트`);
+    document.getElementById('enterBtn').style.visibility = 'visible';
+    document.getElementById('exitBtn').style.visibility = 'hidden';
+    document.getElementById('messages').style.visibility = 'hidden';
+
+    const sessionDto = { meetingId, userId };
+    WebSocketService.sendMessage(`/signal/pub/meeting/${roomId}/leave`, sessionDto);
   };
 
   return (
-    <div>
-      <h1>WebSocket Example</h1>
-      <button onClick={sendMessageToServer}>서버로 메시지 전송</button>
-    </div>
+    <S.Container>
+      <h1>{title}</h1>
+      <S.RoomButtonContainer>
+        <S.EnterButton id="enterBtn" onClick={handleEnter}>
+          방 입장
+        </S.EnterButton>
+        <S.ExitButton id="exitBtn" onClick={handleLeave}>
+          방 나가기
+        </S.ExitButton>
+      </S.RoomButtonContainer>
+      <div>
+        <S.MessageInput
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="메시지를 입력하세요"
+        />
+        <S.SendButton onClick={handleSendMessage}>전송</S.SendButton>
+      </div>
+      <S.MessagesContainer id="messages">
+        <h3>메시지:</h3>
+        <ul>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg.message}</li>
+          ))}
+        </ul>
+      </S.MessagesContainer>
+    </S.Container>
   );
+};
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 100px;
+  `,
+
+  RoomButtonContainer: styled.div`
+    display: flex;
+    gap: 10px;
+    button {
+      padding: 10px 15px;
+      border-radius: 6px;
+    }
+  `,
+
+  EnterButton: styled.button`
+    background-color: var(--blue01);
+    color: var(--white);
+  `,
+  ExitButton: styled.button`
+    background-color: var(--red01);
+    color: var(--white);
+    visibility: hidden;
+  `,
+
+  MessageInput: styled.input`
+    padding: 10px;
+    border: 1px solid var(--gray02);
+    margin: 20px 0;
+  `,
+
+  SendButton: styled.button`
+    background-color: var(--blue02);
+    padding: 11px;
+  `,
+
+  MessagesContainer: styled.div`
+    visibility: hidden;
+  `,
 };
 
 export default KurentoService;
--- src/pages/kurento-service/utils/kurento-service.ts
+++ src/pages/kurento-service/utils/kurento-service.ts
@@ -1,19 +1,48 @@
-const name = null;
-const roomId = null;
-const roomName = null;
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
-function register() {
-  const name = $('#uuid').val();
-  const roomId = $('#roomId').val();
-  roomName = $('#roomName').val();
+const WebSocketService = (() => {
+  let client = null;
 
-  document.getElementById('room-header').innerText = 'ROOM ' + roomName;
-  document.getElementById('room').style.display = 'block';
+  const connect = (onMessageReceived) => {
+    client = new Client({
+      webSocketFactory: () => {
+        return new SockJS('https://api.effi.club/signal/connect');
+      },
+      onConnect: () => {
+        console.log('WebSocket 연결');
+        client.subscribe('/signal/sub/meeting/{roomId}/chat', (message) => {
+          onMessageReceived(JSON.parse(message.body));
+        });
+      },
+      onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+      },
+    });
 
-  const message = {
-    id: 'joinRoom',
-    name: $('#uuid').val(),
-    room: roomId,
+    client.activate();
   };
-  // sendMessageToServer(message);
-}
+
+  const disconnect = () => {
+    if (client !== null) {
+      console.log('STOMP 연결 끊어짐');
+      client.deactivate();
+    }
+  };
+
+  const sendMessage = (destination, body) => {
+    if (client !== null && client.connected) {
+      console.log('서버로부터 메시지 수신:', body);
+      client.publish({ destination, body: JSON.stringify(body) });
+    }
+  };
+
+  return {
+    connect,
+    disconnect,
+    sendMessage,
+  };
+})();
+
+export default WebSocketService;
