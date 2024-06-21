c7932802cbe01525c670042c044e088bd0df7665
Jules <dali1999@naver.com>
2024-06-22 02:17:59 +0900

    feat: roomId 변경

--- src/pages/meeting-room/components/kurento-cameras.tsx
+++ src/pages/meeting-room/components/kurento-cameras.tsx
@@ -26,7 +26,7 @@ const KurentoCameras = () => {
   ]);
 
   const userId = userInfo?.id;
-  const roomId = 111186;
+  const roomId = 111190;
   const memberList = groupInfo?.memberList;
 
   const ws = useRef(null);
